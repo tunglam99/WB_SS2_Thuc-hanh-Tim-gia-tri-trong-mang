703dea7b5e69cba391eb64ceaab09a6d626d913b
Nguyen Tung Lam <lamnthnue.edu.vn@gmail.com>
2019-07-29 02:21:12 +0700

    'ok'

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.awt.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        String[] students = {"Lam","Phong","Camila"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a name  student : ");
+        String inputName =  scanner.nextLine();
+
+        boolean isExit = false;
+        for (int i = 0; i< students.length; i++) {
+            if (students[i].equals(inputName)) {
+                System.out.println("Position of the students in the list " + inputName+ " is " + i);
+                isExit = true;
+                break;
+            }
+        }
+        if (!isExit) {
+            System.out.println("Not found" + inputName+ " in the list");
+        }
+    }
+}
